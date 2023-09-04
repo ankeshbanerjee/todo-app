666e4262a8ebfacff458ed31102763a87451b859
ankeshbanerjee <banerjeeankesh@gmail.com>
2023-09-05 02:33:04 +0530

    added edit task feature in frontend

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,55 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { Context } from "../context/userContext";
 import { Navigate } from "react-router-dom";
 import AddTask from "../components/AddTask";
 import TodoItem from "../components/TodoItem";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const Home = () => {
-  const { isAuth, refresh } = useContext(Context);
+  const { isAuth, refresh, setRefresh } = useContext(Context);
   const [tasks, setTasks] = useState([]);
+
+  // to update task
+  const [newTitle, setNewTitle] = useState("");
+  const [newDesc, setNewDesc] = useState("");
+  const [id, setId] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const ref = useRef();
+  const refClose = useRef();
+
+  const showModal = (title, desc, id) => {
+    ref.current.click();
+    setNewTitle(title);
+    setNewDesc(desc);
+    setId(id);
+  };
+
+  const handleEdit = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.put(
+        `${process.env.REACT_APP_BACKEND_SERVER}/task/editTask/${id}`,
+        {
+          title: newTitle,
+          description: newDesc,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      toast.success(res.data.message);
+      setLoading(false);
+      refClose.current.click();
+      setRefresh((prev) => !prev);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setLoading(false);
+      refClose.current.click();
+      setRefresh((prev) => !prev);
+    }
+  };
 
   useEffect(() => {
     axios
@@ -21,22 +63,103 @@ const Home = () => {
   if (!isAuth) return <Navigate to="/login" />;
 
   return (
-    <div className="container" style={{width: "70%", margin: 'auto'}}>
+    <div className="container" style={{ width: "70%", margin: "auto" }}>
       <AddTask />
       <h3 className="mb-3">Your tasks</h3>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Edit Task
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="newTitle" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="newTitle"
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="newDesc" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="newDesc"
+                    value={newDesc}
+                    onChange={(e) => setNewDesc(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                ref={refClose}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={handleEdit}
+                disabled={
+                  newTitle.length === 0 || newDesc.length === 0 || loading
+                }
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary d-none"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          ref={ref}
+        ></button>
+      </div>
       <div>
-      {!tasks.length && <p>Your tasks will be shown here. Currently no task to show!</p>}
-      {tasks.map((t)=>{
-        return(
-          <TodoItem
-            title={t.title}
-            desc={t.description}
-            isCompleted={t.isCompleted}
-            id={t._id}
-            key={t._id}
-          />
-        )
-      })}
+        {!tasks.length && (
+          <p>Your tasks will be shown here. Currently no task to show!</p>
+        )}
+        {tasks.map((t) => {
+          return (
+            <TodoItem
+              title={t.title}
+              desc={t.description}
+              isCompleted={t.isCompleted}
+              showModal={showModal}
+              id={t._id}
+              key={t._id}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,66 +4,102 @@ import { toast } from "react-hot-toast";
 import { Context } from "../context/userContext";
 
 const TodoItem = (props) => {
-  const [loading, setLoading] = useState(false)
-  const {title, desc, isCompleted, id} = props;
-  const {setRefresh} = useContext(Context);
-
-  const updateTask = ()=>{
-    setLoading(true)
-    axios.put(`${process.env.REACT_APP_BACKEND_SERVER}/task/${id}`, {}, {
-      withCredentials: true
-    }).then((res)=>{
-      toast.success(res.data.message)
-      setRefresh(prev=> !prev);
-      setLoading(false)
-    }).catch((err)=>{
-      toast.error(err.response.data.message);
-      setLoading(false)
-    })
-  }
-
-  const deleteTask = async()=>{
-    setLoading(true)
-    try {
-      const result = await axios.delete(`${process.env.REACT_APP_BACKEND_SERVER}/task/${id}`,{
-        withCredentials: true
+  const [loading, setLoading] = useState(false);
+  const { title, desc, isCompleted, id, showModal } = props;
+  const { setRefresh } = useContext(Context);
+  
+  const updateTask = () => {
+    setLoading(true);
+    axios
+      .put(
+        `${process.env.REACT_APP_BACKEND_SERVER}/task/${id}`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+        setRefresh((prev) => !prev);
+        setLoading(false);
       })
-      toast.success(result.data.message)
-      setRefresh(prev => !prev)
-      setLoading(false)
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        setLoading(false);
+      });
+  };
+
+  const deleteTask = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.delete(
+        `${process.env.REACT_APP_BACKEND_SERVER}/task/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      toast.success(result.data.message);
+      setRefresh((prev) => !prev);
+      setLoading(false);
     } catch (error) {
       toast.error(error.response.data.message);
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
   return (
     <div className="container mb-3">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <h5 className="card-title">{title}</h5>
+            <span
+              style={{
+                cursor: "pointer",
+                marginLeft: "10px",
+                marginBottom: "9px",
+              }}
+              onClick={()=>showModal(title, desc, id)}
+            >
+              <i className="fa-solid fa-pen-to-square fa-lg"></i>
+            </span>
+          </div>
           <div
-            className="container"
-            style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
           >
-            <p className="card-text">
-              {desc}
-            </p>
-            <div style={{display: 'flex', alignItems: 'center', marginLeft: '15px'}}>
-              <div className="form-check" style={{display: 'flex', alignItems: 'center'}}>
+            <p className="card-text">{desc}</p>
+
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div
+                className="form-check"
+                style={{ display: "flex", alignItems: "center" }}
+              >
                 <input
                   className="form-check-input"
                   type="checkbox"
                   value=""
                   id="flexCheckChecked"
                   checked={isCompleted}
-                  onChange={()=>updateTask(id)}
+                  onChange={() => updateTask(id)}
                   disabled={loading}
-                  style={{height: '23px', width: '23px'}}
+                  style={{ height: "23px", width: "23px" }}
                 />
                 <label className="form-check-label" htmlFor="flexCheckChecked">
                   &nbsp;&nbsp;{isCompleted ? "Completed" : "Pending"}
                 </label>
-                <button type="button" className="btn btn-dark" style={{marginLeft: '20px'}} onClick={()=>deleteTask(id)} disabled={loading}>Delete</button>
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  style={{ marginLeft: "20px" }}
+                  onClick={() => deleteTask(id)}
+                  disabled={loading}
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
